6617a7c284eccbc6abe62f97384a91bd8a7a48be
Antoni Garczyński <antek.garczynski@gmail.com>
2024-04-14 23:23:58 +0200

    Update main page

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,29 @@
 import { z, defineCollection } from "astro:content";
+import { format } from "date-fns";
 
 const postsCollection = defineCollection({
   schema: ({ image }) =>
     z.object({
       author: z.string(),
-      date: z.string(),
+      categories: z.array(z.string()),
+      date: z
+        .string()
+        .transform((str) => format(new Date(str), "MMMM d, yyyy")),
+      featured: z.boolean().default(false),
       image: image(),
       title: z.string(),
     }),
 });
 
+const authorsCollection = defineCollection({
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      image: image(),
+    }),
+});
+
 export const collections = {
   posts: postsCollection,
+  authors: authorsCollection,
 };
