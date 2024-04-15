c66f4fd085109ca8d95aa07a44ec605eee89873a
Antoni Garczyński <antek.garczynski@gmail.com>
2024-04-15 22:20:54 +0200

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   output: "server",
+  adapter: netlify(),
 });
